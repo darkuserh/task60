90d9eb4852cb31372779d7f8eb14df4f80dfa729
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-28 19:34:57 +0300

    do a new task

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-$foo='Локальна змінна';
-var_dump($GLOBALS['foo'])
-?>
+$a=5;
+$b=20;
+$sum=$a+$b;
+$sub=$a-$b;
+$mult=$a/$b;
+$div=$a*$b;
+echo '<pre>';
+echo 'Сума даного прикладу : '     .$sum; echo '<br>';
+echo 'Різниця даного прикладу : '  .$sub; echo '<br>';
+echo 'Ділення даного прикладу : '  .$mult; echo '<br>';
+echo 'Добуток даного прикладу : '  .$div; echo '<br>';
+echo '</pre>';
+?>;
