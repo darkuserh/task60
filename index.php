77211c0b787aa89e75b0f3b446871648d4524d28
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-29 02:16:54 +0300

    do a new task

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <?php
-  $min = rand(0, 59);
-  if($min > 15){
-    echo 'Попадає в першу четвертину!';
-  }
-  else if($min > 30){
-    echo 'Попадає в другу четвертину!';
-  }
-  else if($min > 45){
-    echo 'Попадає в третю четвертину!';
-  }
-  else if($min > 59){
-    echo 'Попадає в четверту четвертину!';
-  }
-  else{
-    echo 'Не попадає';
-  }
+define('num1','1');
+define('num2','2');
+define('num3','3');
+define('num4','4');
+$operator=num1;
+  $result = null;
+switch($operator){
+    case num1:
+        $result='Зима';
+        break;
+        case num2:
+            $result='Весна';
+            break;
+            case num3:
+                $result='Літо';
+                break;
+                case num4:
+                    $result='Осінь';
+                    break;
+              default:
+              $result='Помилка вибору';
+}
+if(isset($result)){
+    echo $result;
+}
 ?>
