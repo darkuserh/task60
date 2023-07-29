70cd41ae2a38b3cd128e12ead352bedfde6b9f6e
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-29 18:35:23 +0300

    do a new task

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-$arr=[1, 2, 5, 9, 4, 13, 4, 10];
-foreach($arr as $key=>$element){
-    if($element == 4){
-        echo 'Є число';
-    }else{
-        echo 'Нічого не потрібно робити';
+$sum=0;
+$arr=[1,2,3,4,5,-6,3,-2];
+foreach($arr as $element){
+    if($element>0){
+        $sum+=$element;
     }
+
 }
+print_r($sum)
 ?>;
