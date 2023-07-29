9ea24b399cd2f1202f10f568bfd09e2abf147688
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-29 15:56:20 +0300

    do a new task

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
 <?php
-define('num1','1');
-define('num2','2');
-define('num3','3');
-define('num4','4');
-$operator=num1;
-  $result = null;
-switch($operator){
-    case num1:
-        $result='Зима';
-        break;
-        case num2:
-            $result='Весна';
-            break;
-            case num3:
-                $result='Літо';
-                break;
-                case num4:
-                    $result='Осінь';
-                    break;
-              default:
-              $result='Помилка вибору';
-}
-if(isset($result)){
-    echo $result;
-}
+$arr=['Привіт,','світ','!'];
+echo $arr[0].$arr[1].$arr[2] ;
+
 ?>
