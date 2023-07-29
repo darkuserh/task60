e8311252045d68fe7be0b3375fae463b9ac31ffc
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-29 16:11:33 +0300

    do a new task

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$colors = [
-    'ru' => ['голубой', 'красный', 'зеленый'],
-    'en' => ['blue', 'red', 'green'],
-    ];
-print_r($colors['en'][2])
+$arr = [2 ,5 ,3 ,9];
+$action1=$arr[0]*$arr[1];
+$action2=$arr[2]*$arr[3];
+$result=$action1 + $action2;
+echo $result;
 ?>
