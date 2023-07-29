fa150f22987af730f53058f0712eae5e0aee565a
vysotsk11yu7iy <vysotsk11yu7iy@gmail.com>
2023-07-29 16:17:39 +0300

    do a new task

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$arr = [2 ,5 ,3 ,9];
-$action1=$arr[0]*$arr[1];
-$action2=$arr[2]*$arr[3];
-$result=$action1 + $action2;
-echo $result;
-?>
+$arr = [
+    'cms' =>['joomla', 'wordpress', 'drupal'],
+    'colors' => ['blue'=>'голубой', 'red'=>'красный', 'green'=>'зеленый']
+    ];
+    echo 'Мій любимий колір - ' . $arr['colors']['green'];
+?>;
